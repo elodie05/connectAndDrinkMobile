238370c4999036a3dc196156e5a908288c0fe2c2
elodie05 <elodiecantrel@gmail.com>
2017-06-15 17:09:19 +0200

    Commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,6 +3,29 @@ angular.module('starter.controllers', [])
 .controller('HomeCtrl', function($scope, $http) {
 	
 	//$scope.commands = ['Commande 1','Commande 2','Commande 3'];
+	
+	 	  
+	  /*var req = {
+			  method: 'POST',
+			  url: 'http://localhost:3000/api/command',
+			  headers: {
+				  'Authorization' : 'Bearer '+localStorage.getItem('token')
+			  },
+			  data: JSON.stringify({ ref:'1234',total: '15',user: localStorage.getItem('userId'),
+				  state: 'payee'})
+			  };
+	
+			 
+			 $http(req).then(function(response){
+					  console.log(JSON.stringify(response));
+					  //$scope.commands = response.data
+					 
+				 }, 
+				 function(error){
+					console.log(error)
+				 }
+			);*/
+  
 
 	var req = {
 			  method: 'GET',
@@ -22,13 +45,15 @@ angular.module('starter.controllers', [])
 					console.log(error)
 				 }
 			)
+			
+			
   
 	
 })
 
 .controller('AccountCtrl', function($scope, $http) {
-	console.log('ok')
-	  var req = {
+	console.log('ok');
+	 /* var req = {
 			  method: 'GET',
 			  url: 'http://localhost:3000/api/user',
 			  headers: {
@@ -46,7 +71,7 @@ angular.module('starter.controllers', [])
 					 console.log(JSON.stringify(error));
 					 
 				}
-			 )
+			 )*/
 
 	  /*$scope.save = function(){
 		  var data = {};
@@ -69,44 +94,103 @@ angular.module('starter.controllers', [])
 	  }*/
 })
 
-.controller('ProductsCtrl', function($scope) {
-
+.controller('ProductsCtrl', function($scope,$http) {
+	
+	var req = {
+			  method: 'GET',
+			  url: 'http://localhost:3000/api/item',
+			  headers: {
+				  'Authorization' : 'Bearer '+localStorage.getItem('token')
+			  }
+			 }
+	
+			 
+			 $http(req).then(function(response){
+					  $scope.products = response.data;
+			
+				 }, 
+				 function(error){
+					console.log(error)
+				 }
+			)
+			
+	
 })
 
-.controller('ProductCtrl', function($scope) {
+.controller('ProductCtrl', function($scope,$http,$stateParams) {
+	if(!localStorage.getItem('panier')){
+		console.log('pas panier')
+		var panier = [];localStorage.setItem('panier',panier)
+	}else{
+		var panier =localStorage.getItem('panier');
+	};
+	var item = [];
+	
+	
+	var req = {
+			  method: 'GET',
+			  url: 'http://localhost:3000/api/item/'+$stateParams.id,
+			  headers: {
+				  'Authorization' : 'Bearer '+localStorage.getItem('token')
+			  }
+			 }
+			 $http(req).then(function(response){
+					  $scope.product = response.data;
+			
+				 }, 
+				 function(error){
+					console.log(error)
+				 }
+			)
+			
+	$scope.quantite = 0;
+	
+	console.log('paniiiier',panier);
+	
+	
+	$scope.add = function(){
+		$scope.quantite++;
+		item[$stateParams.id] = $scope.quantite;
 
+		console.log('panier',panier);
+	}
+	$scope.remove = function(){
+		$scope.quantite--;
+		item[$stateParams.id] = $scope.quantite;
+	
+		console.log('panier',panier);
+		//empêcher valeur négatives
+	}
+	
+	panier.push(item)
+	//stocker en local le panier (item + qté)
 })
 
 .controller('ShopCtrl', function($scope) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+	sessionStorage.getItem('panier');
+	console.log(panier);
 
  
 })
 
 .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state,$http,$window) {
-     
+	$scope.data = {};
     $scope.login = function(){
-  	  $scope.data = {};
+  	
   	 
   	  var req = {
   			  method: 'POST',
   			  url: 'http://localhost:3000/authenticate',
-  			  data: JSON.stringify({ username: $scope.data.username, password:$scope.data.username})
+  			  data: { username: $scope.data.username, password: $scope.data.password}
   			  };
   			 
   			 $http(req).then(function(response){
   					   var token = JSON.stringify(response.data.token);
   					   localStorage.setItem("token", token);
   					   
-  					   var userId = JSON.stringify(response.data.iduser);
+  					   var userId = JSON.stringify(response.data.userId);
 					   localStorage.setItem("userId", userId);
-  					   
+					  
   					   $window.location.href = '/#/tab/home';
   					 
   				 }, 
@@ -133,7 +217,7 @@ angular.module('starter.controllers', [])
 	  var req = {
 			  method: 'POST',
 			  url: 'http://localhost:3000/api/user',
-			  data: JSON.stringify({ username: $scope.data.username, password:$scope.data.username, email:$scope.data.email,
+			  data: JSON.stringify({ username: $scope.data.username, password:$scope.data.password, email:$scope.data.email,
 				  address:$scope.data.address, country: $scope.data.city, number:$scope.data.phone})
 			  };
 			 
@@ -144,7 +228,7 @@ angular.module('starter.controllers', [])
 					     template: 'Vous êtes inscrit !'
 					   });
 					   
-					  // $window.location.href = '/#/login.html';
+					   $window.location.href = '/#/login.html';
 					 
 				 }, 
 				 function(error){
